d696e1620bdcc72d1482eaad7444b4b0282ba74a
Jan Janovec <99410175+theHonzic@users.noreply.github.com>
2023-04-08 09:34:32 +0200

    Initial commit

---
+++ NASA API/NASA API/NASA_APIApp.swift
@@ -0,0 +1,17 @@
+//
+//  NASA_APIApp.swift
+//  NASA API
+//
+//  Created by Jan Janovec on 08.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct NASA_APIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
